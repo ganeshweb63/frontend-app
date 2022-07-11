e4aa888dfd09c1263639d851ec5667f89a3bd506
ganeshweb63 <ganeshweb63@gmail.com>
2022-07-12 00:14:29 +0530

    added

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,20 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../components/createContext";
 import './cart.css';
 
 const Cart =()=>{
-    const {cartData , removeCartItem}=useContext(Context);
-    console.log("cartData- cart",cartData);
+    const {cartData , removeCartItem , decrementCartItem , increamentCartItem , totalAmount , getTotalAmount}=useContext(Context);
+  //  console.log("cartData- cart",cartData);
+
+  //  const [cartItems,setCartItems]=useState([]);
+
+    // Updating through local storage Cart information...
+    // useEffect(()=>{
+    //     const CartItemsData= JSON.parse(localStorage.getItem('CartItemsData'));
+    //     if(CartItemsData){
+    //       setCartItems(CartItemsData);
+    //     }
+    
+    //     },[]);
+    //     console.log("cartItems",cartItems);
+    useEffect(()=>{
+        getTotalAmount();
+    },[{cartData , removeCartItem , decrementCartItem , increamentCartItem}]);
 
     return(
         <div>
-            <h1>Cart Products</h1>
-            {cartData.map((item)=>{
+            {cartData.length ===0 ? 
+            <h2>Nothing in Cart page...Please Add Something..</h2>
+             : 
+             cartData.map((item)=>{
                 return <div className="cart-body">
                     <div className="cart-box">
                         <img className='cart-img' src={item.image} alt="product imgage"></img>
                         <h4 className='cart-name'>{item.name}</h4>
-                        <p className='cart-price'>{item.price}</p>
+                        <p className='cart-price'>&#x20B9; {item.price * item.quantity}</p>
+                        <div className="cart-quantity">
+                            <button onClick={()=>decrementCartItem(item._id)} className="amount">-</button>
+                            <span>{item.quantity}</span>
+                            <button  onClick={()=>increamentCartItem(item._id)} className="amount">+</button>
+
+                        </div>
                         {/* <button className='cart-item-remove'>Remove from Cart</button> */}
                         <button onClick={()=>removeCartItem(item._id)} className='cart-item-remove'> <i style={{color:"white"}} class="fa-solid fa-cart-shopping"></i> Remove from Cart</button>
                         <button className='cart-item-buy'><i style={{color:"black"}} class="fa-solid fa-bag-shopping"></i> Buy</button>
@@ -23,7 +46,9 @@ const Cart =()=>{
 
             })}
 
-           
+            <div>
+                <h3>Total : &#x20B9; {totalAmount} </h3>
+            </div>
         </div>
     )
 }
--- src/components/Context.Provider.js
+++ src/components/Context.Provider.js
@@ -17,37 +17,101 @@ function ContextProvider() {
       }
     )
   },[])
-  console.log("products",products);
+ // console.log("products",products);
 
-  // Cart Product code...
+  // Cart add Product code...
 
   const [cartData,setCartData]=useState([]);
 
   const addCartItem=(id)=>{
-   let oneCartItem= products.filter((item)=>{
-      return item._id ===id;
+
+    var check=cartData.every((item)=>{
+      return item._id !== id;
     })
-    console.log("oneCartItem",oneCartItem[0]);
-    setCartData([...cartData,oneCartItem[0]]);
+
+    if(check){
+      let oneCartItem= products.filter((item)=>{
+        return item._id ===id;
+      })
+     // console.log("oneCartItem",oneCartItem[0]);
+      setCartData([...cartData, oneCartItem[0]]);
+    }
+    else{
+      alert("This Product has been added to Cart Already.")
+    }
+
+
   }
+
+
 
   console.log("cartData",cartData);
 
   // remove cart code....
 
   const removeCartItem=(id)=>{
-    let oneCartItem=cartData.filter((item)=>{
-      return item._id !== id;
-    })
-
-    setCartData(oneCartItem);
-
+    if(window.confirm("Do you want to delete this product?")){
+      let oneCartItem=cartData.filter((item)=>{
+        return item._id !== id;
+      })
+  
+      setCartData([...oneCartItem]);  
+    }
+    
   }
 
+  // Cart item decrement ....
+
+  const decrementCartItem=(id)=>{
+      cartData.forEach((item)=>{
+        if(item._id === id){
+          item.quantity ===1 ? item.quantity =1 : item.quantity -=1;
+        }
+      })
+      setCartData([...cartData]);
+  }
+
+   // Cart item Increament ....
+
+   const increamentCartItem=(id)=>{
+   cartData.forEach((item)=>{
+      if(item._id === id){
+       return item.quantity +=1;
+      }  
+    })
+    
+     setCartData([...cartData]);  
+   }
+
+
+  // Cart Products all amount...
+
+    const [totalAmount,setTotalAmount]=useState(0);
+    const getTotalAmount=()=>{
+      let amount=cartData.reduce((prev, item)=>{
+        return prev + (item.price * item.quantity);
+      },0)
+      setTotalAmount(amount);
+    }
+
+    //  Updating through local storage Cart information...
+        useEffect(()=>{
+          const CartItemsData= JSON.parse(localStorage.getItem('CartItemsData'));
+          if(CartItemsData){
+            setCartData(CartItemsData);
+          }
+      
+          },[]);
+
+
+    useEffect(()=>{
+      localStorage.setItem('CartItemsData',JSON.stringify(cartData));
+    },[cartData]);
+    
 
 
   return (
-    <Context.Provider value={{products , cartData , addCartItem , removeCartItem}}>
+    <Context.Provider value={{products , cartData , addCartItem , removeCartItem , decrementCartItem , increamentCartItem , totalAmount , getTotalAmount}}>
       <Navbar/>
     </Context.Provider>
   );
